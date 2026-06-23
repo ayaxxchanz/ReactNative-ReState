c65d40a0cd6c34ea5f8332b8f42696a22997dc60
ayaxxchanz <aliya.genius@gmail.com>
2026-06-24 01:51:03 +0800

    feat: home header

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,15 +1,33 @@
-import { Text, View } from "react-native";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
+import { Image, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="font-rubik-bold text-2xl my-10">Welcome to ReState</Text>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <View className="px-5">
+        {/* Header */}
+        <View className="flex flex-row items-center justify-between mt-5">
+          <View className="flex flex-row items-center">
+
+            <Image source={images.avatar} className="w-12 h-12 rounded-full" />
+
+            <View className="flex flex-col ml-2">
+              <Text className="text-xs font-rubik text-black-100">
+                Good Morning
+              </Text>
+
+              <Text className="font-rubik-medium text-base text-black-300">
+                Aliya H.
+              </Text>
+            </View>
+          </View>
+
+          <Image source={icons.bell} className="w-5 h-5" />
+        </View>
+      </View>
+
+    </SafeAreaView>
   );
 }
