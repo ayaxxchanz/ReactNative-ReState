e064f8701eb05f15fd967b1fe30f5ca728318018
ayaxxchanz <aliya.genius@gmail.com>
2026-06-24 01:57:16 +0800

    feat: home reusable cards

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,7 +1,8 @@
+import { Card, FeaturedCard } from "@/components/Cards";
 import Search from "@/components/Search";
 import icons from "@/constants/icons";
 import images from "@/constants/images";
-import { Image, Text, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
@@ -28,9 +29,36 @@ export default function Index() {
           <Image source={icons.bell} className="w-5 h-5" />
         </View>
 
-
         <Search />
-        
+
+        {/* Featured */}
+        <View className="my-5">
+          <View className="flex flex-row items-center justify-between">
+            <Text className="text-xl font-rubik-bold text-black-300">Featured</Text>
+            <TouchableOpacity>
+              <Text className="text-base font-rubik-bold text-primary-300">See All</Text>
+            </TouchableOpacity>
+          </View>
+
+          {/* Featured Card */}
+          <View className="flex flex-row gap-5 mt-5">
+            <FeaturedCard />
+            <FeaturedCard />
+          </View>
+
+          <View className="flex flex-row items-center justify-between mt-5">
+            <Text className="text-xl font-rubik-bold text-black-300">Our Recommendation</Text>
+            <TouchableOpacity>
+              <Text className="text-base font-rubik-bold text-primary-300">See All</Text>
+            </TouchableOpacity>
+          </View>
+
+          {/* Card */}
+          <View className="flex flex-row gap-5 mt-5">
+            <Card />
+            <Card />
+          </View>
+        </View>
       </View>
 
     </SafeAreaView>
