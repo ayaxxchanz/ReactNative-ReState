35cf03d6b7ec86b4ac980cd7f9a0fc10cc98afea
ayaxxchanz <aliya.genius@gmail.com>
2026-06-24 19:37:12 +0800

    chore: database seeding

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -3,7 +3,8 @@ import Filters from "@/components/Filters";
 import Search from "@/components/Search";
 import icons from "@/constants/icons";
 import { useGlobalContext } from "@/lib/global-provider";
-import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
+import seed from "@/lib/seed";
+import { Button, FlatList, Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
@@ -11,6 +12,7 @@ export default function Index() {
 
   return (
     <SafeAreaView className="bg-white h-full">
+      <Button onPress={seed} title="Seed" />
       <FlatList
         data={[1, 2, 3, 4]}
         renderItem={({ item }) => <Card />}
