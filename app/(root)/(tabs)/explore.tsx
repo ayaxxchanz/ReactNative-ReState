7d3dd7042d76ceaab5d1c4843c03087c9d2fc26b
ayaxxchanz <aliya.genius@gmail.com>
2026-06-26 04:28:30 +0800

    feat: explore screen

--- app/(root)/(tabs)/explore.tsx
+++ app/(root)/(tabs)/explore.tsx
@@ -1,12 +1,78 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import { Card } from "@/components/Cards";
+import Filters from "@/components/Filters";
+import NoResults from "@/components/NoResults";
+import Search from "@/components/Search";
+import icons from "@/constants/icons";
+import { getProperties } from "@/lib/appwrite";
+import { useAppwrite } from "@/lib/useAppwrite";
+import { router, useLocalSearchParams } from "expo-router";
+import { useEffect } from "react";
+import { ActivityIndicator, FlatList, Image, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-const explore = () => {
+export default function Explore() {
+  const params = useLocalSearchParams<{ query?: string, filter?: string }>()
+
+  const handleCardPress = (id: string) => router.push(`/properties/${id}`)
+
+  const { data: properties, loading, refetch } = useAppwrite({
+    fn: getProperties,
+    params: {
+      filter: params.filter!,
+      query: params.query!,
+      limit: 20
+    },
+    skip: true
+  })
+
+  useEffect(() => {
+    refetch({
+      filter: params.filter!,
+      query: params.query!,
+      limit: 20
+    })
+  }, [params.filter, params.query])
+
   return (
-    <View>
-      <Text>explore</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="bg-white h-full">
+      <FlatList
+        data={properties}
+        numColumns={2}
+        renderItem={({ item }) => (
+          <Card item={item} onPress={() => handleCardPress(item.$id)} />
+        )}
+        keyExtractor={(item) => item.$id}
+        contentContainerClassName="pb-32"
+        columnWrapperClassName="flex gap-5 px-5"
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={
+          loading ? (
+            <ActivityIndicator size="large" className="text-primary-300 mt-5" />
+          ) : <NoResults />
+        }
+        ListHeaderComponent={
+          <View className="px-5">
+            <View className="flex flex-row items-center justify-between mt-5">
+              <TouchableOpacity onPress={() => router.back()} className="flex flex-row rounded-full bg-primary-200 items-center justify-center size-11">
+                <Image source={icons.backArrow} className="w-5 h-5" />
+              </TouchableOpacity>
+              <Text className="font-rubik-medium text-base text-center text-black-300">Search For Your Idea Home</Text>
+              <Image source={icons.bell} className="w-5 h-5" />
+            </View>
+            <Search />
 
-export default explore
+            <View className="mt-5">
+              <Filters />
+              <Text className="text-xl font-rubik-bold text-black-300 mt-5">
+                Found {properties?.length} Properties
+                </Text>
+            </View>
+          </View>
+        }
+
+      />
+
+
+    </SafeAreaView>
+  );
+}
